9f3f9d064f58014d1473850c766a0104683fa39e
AryMos <arielsantiagomosciaro@gmail.com>
2026-06-13 11:33:35 -0300

    CONFIGURANDO STATIC_ROOT

--- configuracion/settings.py
+++ configuracion/settings.py
@@ -136,7 +136,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Configuración para el envío de correos electrónicos (Gmail)
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
